34d39ea1d6a9382d15fb017198fb377287f3ba43
David Asaolu <asaoludavid@gmail.com>
2021-03-14 12:53:48 -0230

    first commit

---
+++ models/postMessage.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    lastName: String,
+    firstName: String,
+    gender: String,
+    group: String,
+    caucassian: String,
+    newcomer: String,
+    
+    forFamily: String,
+    title: String,
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
